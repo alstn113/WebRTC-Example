4ea85aa66a3312baebd3782d94a5f45e4b496509
neko113 <alstn113@gmail.com>
2022-10-10 04:58:40 +0900

    feat: ticket api

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,29 +11,192 @@ use serde::{Deserialize, Serialize};
 use std::fmt::Display;
 use std::sync::Mutex;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+#[derive(Serialize, Deserialize)]
+struct Ticket {
+    id: u32,
+    author: String,
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+// Implement Responder Trait for Ticket
+impl Responder for Ticket {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        let res_body = serde_json::to_string(&self).unwrap();
+
+        // Create HttpResponse and set Content Type
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(res_body)
+    }
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[derive(Debug, Serialize)]
+struct ErrNoId {
+    id: u32,
+    err: String,
+}
+
+// Implement ResponseError for ErrNoId
+impl ResponseError for ErrNoId {
+    fn status_code(&self) -> StatusCode {
+        StatusCode::NOT_FOUND
+    }
+
+    fn error_response(&self) -> HttpResponse<BoxBody> {
+        let body = serde_json::to_string(&self).unwrap();
+        let res = HttpResponse::new(self.status_code());
+        res.set_body(BoxBody::new(body))
+    }
+}
+
+// Implement Display for ErrNoId
+impl Display for ErrNoId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+struct AppState {
+    tickets: Mutex<Vec<Ticket>>,
+}
+
+// Create a ticket
+#[post("/tickets")]
+async fn post_ticket(req: web::Json<Ticket>, data: web::Data<AppState>) -> impl Responder {
+    let new_ticket = Ticket {
+        id: req.id,
+        author: String::from(&req.author),
+    };
+
+    let mut tickets = data.tickets.lock().unwrap();
+
+    let response = serde_json::to_string(&new_ticket).unwrap();
+
+    tickets.push(new_ticket);
+    HttpResponse::Created()
+        .content_type(ContentType::json())
+        .body(response)
+}
+
+// Get all tickets
+#[get("/tickets")]
+async fn get_tickets(data: web::Data<AppState>) -> impl Responder {
+    let tickets = data.tickets.lock().unwrap();
+
+    let response = serde_json::to_string(&(*tickets)).unwrap();
+
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(response)
+}
+
+// Get a ticket with the corresponding id
+#[get("/tickets/{id}")]
+async fn get_ticket(id: web::Path<u32>, data: web::Data<AppState>) -> Result<Ticket, ErrNoId> {
+    let ticket_id: u32 = *id;
+    let tickets = data.tickets.lock().unwrap();
+
+    let ticket: Vec<_> = tickets.iter().filter(|x| x.id == ticket_id).collect();
+
+    if !ticket.is_empty() {
+        Ok(Ticket {
+            id: ticket[0].id,
+            author: String::from(&ticket[0].author),
+        })
+    } else {
+        let response = ErrNoId {
+            id: ticket_id,
+            err: String::from("ticket not found"),
+        };
+        Err(response)
+    }
+}
+
+// Update the ticket with the corresponding id
+#[put("/tickets/{id}")]
+async fn update_ticket(
+    id: web::Path<u32>,
+    req: web::Json<Ticket>,
+    data: web::Data<AppState>,
+) -> Result<HttpResponse, ErrNoId> {
+    let ticket_id: u32 = *id;
+
+    let new_ticket = Ticket {
+        id: req.id,
+        author: String::from(&req.author),
+    };
+
+    let mut tickets = data.tickets.lock().unwrap();
+
+    let id_index = tickets.iter().position(|x| x.id == ticket_id);
+
+    match id_index {
+        Some(id) => {
+            let response = serde_json::to_string(&new_ticket).unwrap();
+            tickets[id] = new_ticket;
+            Ok(HttpResponse::Ok()
+                .content_type(ContentType::json())
+                .body(response))
+        }
+        None => {
+            let response = ErrNoId {
+                id: ticket_id,
+                err: String::from("ticket not found"),
+            };
+            Err(response)
+        }
+    }
+}
+
+// Delete the ticket with the corresponding id
+#[delete("/tickets/{id}")]
+async fn delete_ticket(id: web::Path<u32>, data: web::Data<AppState>) -> Result<Ticket, ErrNoId> {
+    let ticket_id: u32 = *id;
+    let mut tickets = data.tickets.lock().unwrap();
+
+    let id_index = tickets.iter().position(|x| x.id == ticket_id);
+
+    match id_index {
+        Some(id) => {
+            let deleted_ticket = tickets.remove(id);
+            Ok(deleted_ticket)
+        }
+        None => {
+            let response = ErrNoId {
+                id: ticket_id,
+                err: String::from("ticket not found"),
+            };
+            Err(response)
+        }
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let app_state = web::Data::new(AppState {
+        tickets: Mutex::new(vec![
+            Ticket {
+                id: 1,
+                author: String::from("Jane Doe"),
+            },
+            Ticket {
+                id: 2,
+                author: String::from("Patrick Star"),
+            },
+        ]),
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .app_data(app_state.clone())
+            .service(post_ticket)
+            .service(get_ticket)
+            .service(get_tickets)
+            .service(update_ticket)
+            .service(delete_ticket)
     })
-    .bind(("localhost", 8080))?
+    .bind(("localhost", 8000))?
     .run()
     .await
 }
