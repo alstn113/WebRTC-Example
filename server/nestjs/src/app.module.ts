756b85a1d830fb8775f148257985194945e53ada
neko113 <alstn113@gmail.com>
2022-10-12 00:13:26 +0900

    feat: add primsa orm

--- server/nestjs/src/app.module.ts
+++ server/nestjs/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
 
 import configuration from '~/config/configuration';
+import { PrismaModule } from '~/prisma/prisma.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import configuration from '~/config/configuration';
       isGlobal: true,
       load: [configuration],
     }),
+    PrismaModule,
   ],
   providers: [
     {
