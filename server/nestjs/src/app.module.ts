e3f8c95d94b1ed4457ae761786629a2aa4d4e239
neko113 <alstn113@gmail.com>
2022-10-11 23:56:41 +0900

    feat: add config

--- server/nestjs/src/app.module.ts
+++ server/nestjs/src/app.module.ts
@@ -1,8 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
+
+import configuration from '~/config/configuration';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
--- server/nestjs/src/main.ts
+++ server/nestjs/src/main.ts
@@ -1,8 +1,26 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+
+import { AppModule } from '~/app.module';
+import { setupSwagger } from '~/utils/setupSwagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('server.port');
+  const logger = new Logger();
+
+  app.setGlobalPrefix('/api');
+  app.use(cookieParser());
+  app.enableCors({
+    origin: configService.get<string>('client'),
+    credentials: true,
+  });
+
+  setupSwagger(app);
+  await app.listen(port);
+  logger.verbose(`Listening On Port ${port}`);
 }
 bootstrap();
