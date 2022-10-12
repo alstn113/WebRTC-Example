3e817ca6d20ebc2cca5172c7eabc695486a13eec
neko113 <alstn113@gmail.com>
2022-10-12 20:17:59 +0900

    feat: add EventsGateway method

--- server/nestjs/src/adapters/socke-io.adapter.ts
+++ server/nestjs/src/adapters/socke-io.adapter.ts
@@ -1,7 +1,8 @@
 import { IoAdapter } from '@nestjs/platform-socket.io';
+import { ServerOptions } from 'socket.io';
 
 export class SocketIoAdapter extends IoAdapter {
-  createIOServer(port: number, options?: any): any {
+  createIOServer(port: number, options?: ServerOptions): any {
     const server = super.createIOServer(port, options);
 
     return server;
--- server/nestjs/src/modules/events/events.gateway.ts
+++ server/nestjs/src/modules/events/events.gateway.ts
@@ -1,15 +1,38 @@
+import { Logger } from '@nestjs/common';
 import {
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class EventsGateway {
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class EventsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer()
   server: Server;
+  private logger = new Logger('EventsGateway');
+
+  afterInit() {
+    this.logger.log('Init');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client Disconnected : ${client.id}`);
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Client Connected : ${client.id}`);
+  }
 
   @SubscribeMessage('ClientToServer')
   async handleMessage(@MessageBody('data') data: string) {
