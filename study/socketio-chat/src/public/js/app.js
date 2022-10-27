5cd60379393bf7b4789b5f4f9d7b9d720b736960
neko113 <alstn113@gmail.com>
2022-10-27 20:35:32 +0900

    ~2.6 room notifications

--- study/socketio-chat/src/public/js/app.js
+++ study/socketio-chat/src/public/js/app.js
@@ -1,18 +1,27 @@
 const socket = io();
 
 const welcome = document.getElementById("welcome");
-const form = document.querySelector("form");
+const form = welcome.querySelector("form");
 const room = document.getElementById("room");
 
 room.hidden = true;
 
 let roomName;
 
-function handleSubmit(event) {
+function addMessage(message) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
+}
+
+function handleMessageSubmit(event) {
   event.preventDefault();
-  const input = form.querySelector("input");
-  socket.emit("enter_room", input.value, showRoom);
-  roomName = input.value;
+  const input = room.querySelector("input");
+  const value = input.value;
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
   input.value = "";
 }
 
@@ -21,6 +30,28 @@ function showRoom() {
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
 }
 
-form.addEventListener("submit", handleSubmit);
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", () => {
+  addMessage("someone joined!");
+});
+
+socket.on("bye", () => {
+  addMessage("someone left ㅠㅠ");
+});
+
+socket.on("new_message", (msg) => {
+  addMessage(msg);
+});
--- study/socketio-chat/src/server.js
+++ study/socketio-chat/src/server.js
@@ -1,6 +1,6 @@
-import express from "express";
 import http from "http";
 import SocketIO from "socket.io";
+import express from "express";
 
 const app = express();
 
@@ -10,16 +10,26 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const server = http.createServer(app);
-const io = SocketIO(server);
-
-io.on("connection", (socket) => {
+wsServer.on("connection", (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
+  });
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+  });
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", msg);
     done();
   });
 });
 
-server.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
