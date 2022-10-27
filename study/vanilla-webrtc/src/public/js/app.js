50ebed89d35a51b961782b0044768a28047d461c
neko113 <alstn113@gmail.com>
2022-10-27 14:29:34 +0900

    ~1.6 websocket chat complete

---
+++ study/vanilla-webrtc/src/public/js/app.js
@@ -0,0 +1,26 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function handleOpen() {
+  console.log("Connected to Server ✅");
+}
+socket.addEventListener("close", () => {
+  console.log("Connected to Server ✅");
+});
+socket.addEventListener("open", handleOpen);
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data);
+});
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- study/vanilla-webrtc/src/server.js
+++ study/vanilla-webrtc/src/server.js
@@ -1,13 +1,28 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
-
 const app = express();
-
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+function onSocketClose() {
+  console.log("Disconnected from the Browser ❌");
+}
 
-const handleListening = () =>
-  console.log("Listening on: http://localhost:3000");
-app.listen(3000, handleListening);
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  console.log("Connected to Browser ✅");
+  socket.on("close", onSocketClose);
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message));
+  });
+});
+
+server.listen(3000, handleListen);
