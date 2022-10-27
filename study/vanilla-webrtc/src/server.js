fb7b1b807e204c464bb61be98a5e96ee8100075f
neko113 <alstn113@gmail.com>
2022-10-27 13:06:10 +0900

    ~0.4 front setup

--- study/vanilla-webrtc/src/server.js
+++ study/vanilla-webrtc/src/server.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListening = () =>
+  console.log("Listening on: http://localhost:3000");
+app.listen(3000, handleListening);
