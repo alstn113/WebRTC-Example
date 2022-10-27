77905cdd3caf125453b90c01abc5d5f919a35bf9
neko113 <alstn113@gmail.com>
2022-10-27 18:41:33 +0900

    ~2.4 socketio rooms

--- study/socketio-chat/src/public/js/app.js
+++ study/socketio-chat/src/public/js/app.js
@@ -1,1 +1,26 @@
 const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = document.querySelector("form");
+const room = document.getElementById("room");
+
+room.hidden = true;
+
+let roomName;
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
+  input.value = "";
+}
+
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+}
+
+form.addEventListener("submit", handleSubmit);
--- study/socketio-chat/src/server.js
+++ study/socketio-chat/src/server.js
@@ -16,7 +16,10 @@ const server = http.createServer(app);
 const io = SocketIO(server);
 
 io.on("connection", (socket) => {
-  console.log(socket);
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+  });
 });
 
 server.listen(3000, handleListen);
