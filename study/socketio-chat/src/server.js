dc3b66f6d7747cb9ca70c371219b215ba4fcc9f4
neko113 <alstn113@gmail.com>
2022-10-27 15:30:33 +0900

    ~2.1 socketio install

---
+++ study/socketio-chat/src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import http from "http";
+import SocketIO from "socket.io";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const io = SocketIO(server);
+
+io.on("connection", (socket) => {
+  console.log(socket);
+});
+
+server.listen(3000, handleListen);
