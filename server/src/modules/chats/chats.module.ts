577db2898e3693b0e0654ca1d0089f13fa37c2cc
neko113 <alstn113@gmail.com>
2022-11-07 19:40:51 +0900

    chore: server put chats service into providers

--- server/src/modules/chats/chats.module.ts
+++ server/src/modules/chats/chats.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '~/prisma/prisma.service';
 import { ChatsController } from './chats.controller';
 import { ChatsGateway } from './chats.gateway';
+import { ChatsService } from './chats.service';
 
 @Module({
   controllers: [ChatsController],
-  providers: [PrismaService, ChatsGateway],
+  providers: [PrismaService, ChatsGateway, ChatsService],
 })
 export class ChatsModule {}
