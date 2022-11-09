fdc20c2d2509700b699808ea8a8b928cdf013bd1
neko113 <alstn113@gmail.com>
2022-11-09 21:04:46 +0900

    refactor: auth.service.ts property

--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -5,6 +5,12 @@ import { PrismaService } from '~/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+  readonly ACCESS_TOKEN_SECRET = this.configService.get<string>('auth.access_token_secret');
+  readonly ACCESS_TOKEN_EXPIRE = this.configService.get<string>('auth.access_token_expire');
+
+  readonly REFRESH_TOKEN_SECRET = this.configService.get<string>('auth.refresh_token_secret');
+  readonly REFRESH_TOKEN_EXPIRE = this.configService.get<string>('auth.refresh_token_expire');
+
   constructor(
     private readonly prisma: PrismaService,
     private readonly jwtService: JwtService,
@@ -12,28 +18,22 @@ export class AuthService {
   ) {}
 
   async getAccessToken(userId: number, email: string) {
-    const ACCESS_TOKEN_SECRET = this.configService.get<string>('auth.access_token_secret');
-    const ACCESS_TOKEN_EXPIRE = this.configService.get<string>('auth.access_token_expire');
-
     const payload = { sub: 'access_token', userId, email };
 
     const accessToken = await this.jwtService.signAsync(payload, {
-      secret: ACCESS_TOKEN_SECRET,
-      expiresIn: ACCESS_TOKEN_EXPIRE,
+      secret: this.ACCESS_TOKEN_SECRET,
+      expiresIn: this.ACCESS_TOKEN_EXPIRE,
     });
 
     return accessToken;
   }
 
   async getRefreshToken(userId: number, email: string) {
-    const REFRESH_TOKEN_SECRET = this.configService.get<string>('auth.refresh_token_secret');
-    const REFRESH_TOKEN_EXPIRE = this.configService.get<string>('auth.refresh_token_expire');
-
     const payload = { sub: 'refresh_token', userId, email };
 
     const refresh_token = await this.jwtService.signAsync(payload, {
-      secret: REFRESH_TOKEN_SECRET,
-      expiresIn: REFRESH_TOKEN_EXPIRE,
+      secret: this.REFRESH_TOKEN_SECRET,
+      expiresIn: this.REFRESH_TOKEN_EXPIRE,
     });
 
     return refresh_token;
@@ -54,10 +54,8 @@ export class AuthService {
   }
 
   async verifyRefreshToken(refreshToken: string) {
-    const REFRESH_TOKEN_SECRET = this.configService.get<string>('auth.refresh_token_secret');
-
     const payload = await this.jwtService.verifyAsync(refreshToken, {
-      secret: REFRESH_TOKEN_SECRET,
+      secret: this.REFRESH_TOKEN_SECRET,
     });
 
     return payload;
