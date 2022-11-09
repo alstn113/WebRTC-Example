02b69bffa4798e0864b8c2261c87550edea032c5
neko113 <alstn113@gmail.com>
2022-11-10 01:19:29 +0900

    feat: chats gateways 틀

--- server/src/modules/chats/chats.gateway.ts
+++ server/src/modules/chats/chats.gateway.ts
@@ -8,20 +8,18 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { Namespace, Server, Socket } from 'socket.io';
+import { AddMessageDto } from './dto/add-message.dto';
 
-@WebSocketGateway(8001, {
-  cors: 'http://localhost:3000',
+@WebSocketGateway({
   namespace: 'chats',
+  cors: { origin: ['http://localhost:3000'] },
 })
-export class ChatsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+export class ChatsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
-  server: Server;
-  private logger = new Logger('ChatsGateway');
-
-  afterInit() {
-    this.logger.verbose('Init');
-  }
+  private readonly nsp: Namespace;
+  private readonly connectedUsers: Map<string, string> = new Map();
+  private readonly logger = new Logger('ChatsGateway');
 
   handleConnection(client: Socket) {
     this.logger.verbose(`Client Connected : ${client.id}`);
@@ -31,9 +29,26 @@ export class ChatsGateway implements OnGatewayInit, OnGatewayConnection, OnGatew
     this.logger.verbose(`Client Disconnected : ${client.id}`);
   }
 
-  @SubscribeMessage('ClientToServer')
-  async handleMessage(@MessageBody('data') data: string) {
-    console.log(data);
-    this.server.emit('ServerToClient', data + ' from server');
+  @SubscribeMessage('message')
+  async onMessage(client: Socket, addMessageDto: AddMessageDto) {
+    this.logger.verbose(`Message Received : ${addMessageDto.text}`);
   }
+
+  @SubscribeMessage('join')
+  async onJoin(client: Socket, room: string) {
+    client.join(room);
+    this.logger.verbose(`Client ${client.id} joined room ${room}`);
+  }
+
+  @SubscribeMessage('leave')
+  async onLeave(client: Socket, room: string) {
+    client.leave(room);
+    this.logger.verbose(`Client ${client.id} left room ${room}`);
+  }
+
+  // @SubscribeMessage('ClientToServer')
+  // async handleMessage(@MessageBody('data') data: string) {
+  //   console.log(data);
+  //   this.nsp.adapter.emit('ServerToClient', data + ' from server');
+  // }
 }
