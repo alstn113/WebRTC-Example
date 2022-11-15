334af5bc11aa0e857bd42fd00e66a1ec35f0a3e7
neko113 <alstn113@gmail.com>
2022-11-16 08:26:55 +0900

    방학까지 잠정 중단...

---
+++ client/src/libs/utils/PeerConnection.ts
@@ -0,0 +1,28 @@
+import { io, Socket } from 'socket.io-client';
+import { PROPERTIES } from '../../constants/properties';
+
+class PeerConnection {
+  peerConnections: { [key: string]: RTCPeerConnection } = {};
+  senders: RTCRtpSender[] = [];
+  listeners = {};
+  socket: Socket;
+
+  constructor(socket: Socket) {
+    this.socket = socket;
+  }
+
+  addPeerConnection(id: string, stream: MediaStream, callback: Function) {
+    this.peerConnections[id] = new RTCPeerConnection({
+      iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+    });
+
+    stream.getTracks().forEach((track: MediaStreamTrack) => {
+      this.senders.push(this.peerConnections[id].addTrack(track, stream));
+    });
+  }
+}
+
+export const createPeerConnectionContext = () => {
+  const socket = io(PROPERTIES.BASE_URL);
+  return new PeerConnection(socket);
+};
--- server/src/modules/rooms/rooms.gateway.ts
+++ server/src/modules/rooms/rooms.gateway.ts
@@ -1,19 +1,14 @@
 import { Logger } from '@nestjs/common';
 import {
-  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
   OnGatewayInit,
-  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { AuthService } from '../auth/auth.service';
 import { UsersService } from '../users/users.service';
-import { AddMessageDto } from './dto/add-message.dto';
-import { JoinRoomDto } from './dto/join-room.dto';
-import { LeaveRoomDto } from './dto/leave-room.dto';
 
 @WebSocketGateway({
   namespace: 'chats',
@@ -22,7 +17,7 @@ import { LeaveRoomDto } from './dto/leave-room.dto';
 export class ChatsGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
   private connectedUsers: Map<string, number> = new Map();
-  private logger = new Logger('ChatsGateway');
+  private logger = new Logger('RoomsGateway');
 
   constructor(
     private readonly authService: AuthService,
@@ -54,23 +49,5 @@ export class ChatsGateway implements OnGatewayInit, OnGatewayConnection, OnGatew
   handleDisconnect(client: Socket) {
     this.connectedUsers.delete(client.id);
     this.logger.verbose(`Client Disconnected : ${client.id}`);
-  }
-
-  @SubscribeMessage('message')
-  async onMessage(client: Socket, addMessageDto: AddMessageDto) {
-    client.to(addMessageDto.roomId).emit('message', addMessageDto.text);
-    this.logger.verbose(`Message Received : ${addMessageDto.text}`);
-  }
-
-  @SubscribeMessage('join')
-  async onRoomJoin(client: Socket, joinRoomDto: JoinRoomDto) {
-    client.join(joinRoomDto.roomId);
-    this.logger.verbose(`Client ${client.id} joined room ${joinRoomDto.roomId}`);
-  }
-
-  @SubscribeMessage('leave')
-  async onRoomLeave(client: Socket, leaveRoomDto: LeaveRoomDto) {
-    client.leave(leaveRoomDto.roomId);
-    this.logger.verbose(`Client ${client.id} left room ${leaveRoomDto.roomId}`);
   }
 }
