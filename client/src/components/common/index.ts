4fd1951f012e6843f226f4fe049b8a8eb7604714
김민수 MINSOO KIM <alstn113@gmail.com>
2022-12-15 23:20:44 +0900

    feat: add asyncboundary and errorfallback

--- client/src/components/common/index.ts
+++ client/src/components/common/index.ts
@@ -1,5 +1,6 @@
 /** @Components */
 export { default as Button } from './Button/Button';
 export { default as Modal } from './Modal/Modal';
+export { default as Loader } from './Loader/Loader';
 /** @Layouts */
 export { default as Spacer } from './Spacer/Spacer';
