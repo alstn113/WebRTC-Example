94cfee7cfbaf412d4ea4514864a3b58abb9d20af
김민수 MINSOO KIM <alstn113@gmail.com>
2022-12-16 09:10:50 +0900

    feat: add errorboundary

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,37 @@
+// react
 import { Route, Routes } from 'react-router-dom';
 
+// ErrorBoundary
+import ErrorBoundary from '~/components/ErrorBoundary';
+import ErrorFallback from '~/components/ErrorFallback';
+import { MESSAGE } from '~/constants/messages';
+
 // layouts
-import MainLayout from './components/Layouts/MainLayout';
+import MainLayout from '~/components/Layouts/MainLayout';
 
 // pages
-import Home from './pages/Home/Home';
-import Room from './pages/Room/Room';
-import Loading from './pages/Loading/Loading';
-import NotFound from './pages/NotFound/NotFound';
+import Home from '~/pages/Home/Home';
+import Room from '~/pages/Room/Room';
+import Loading from '~/pages/Loading/Loading';
+import NotFound from '~/pages/NotFound/NotFound';
 
 const App = () => {
   return (
-    <Routes>
-      {/* public routes */}
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/room" element={<Room />}>
-          <Route path=":roomId" element={<Room />} />
+    <ErrorBoundary fallback={<ErrorFallback message={MESSAGE.ERROR.UNKNOWN} />}>
+      <Routes>
+        {/* public routes */}
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/room" element={<Room />}>
+            <Route path=":roomId" element={<Room />} />
+          </Route>
+          <Route path="/loading" element={<Loading />} />
         </Route>
-        <Route path="/loading" element={<Loading />} />
-      </Route>
 
-      {/* catch all */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+        {/* catch all */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </ErrorBoundary>
   );
 };
 
