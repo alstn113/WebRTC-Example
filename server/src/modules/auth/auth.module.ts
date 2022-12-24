5b1ea7d8e93cff60f1a745ef4aa1a830f93de5c6
김민수 MINSOO KIM <alstn113@gmail.com>
2022-12-24 20:04:45 +0900

    feat: set PrismaModule global

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PrismaService } from '~/prisma/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, PrismaService],
+  providers: [AuthService],
   exports: [AuthService],
 })
 export class AuthModule {}
--- server/src/modules/users/users.module.ts
+++ server/src/modules/users/users.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PrismaService } from '~/prisma/prisma.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  providers: [UsersService],
 })
 export class UsersModule {}
