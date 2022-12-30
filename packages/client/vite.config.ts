fb553db7e155ee2dc725f4ee35381eacaeff5ed2
김민수 MINSOO KIM <alstn113@gmail.com>
2022-12-30 23:45:24 +0900

    fix: storybook node verison error

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -4,5 +4,13 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
