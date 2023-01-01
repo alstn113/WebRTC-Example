f64f16934c90d574506a9f6a5d78a9a97d29ef7f
김민수 MINSOO KIM <alstn113@gmail.com>
2023-01-02 01:59:54 +0900

    feat: usePeerConnecton hook 틀잡기

--- packages/client/src/hooks/usePeerConnection.ts
+++ packages/client/src/hooks/usePeerConnection.ts
@@ -1,5 +1,22 @@
-const usePeerConnection = () => {
-  return;
+import { useEffect } from 'react';
+import { Socket } from 'socket.io-client';
+import { EVENT } from '~/constants';
+import PeerConnection from '~/lib/sockets/PeerConnection';
+
+const usePeerConnection = (socket: Socket) => {
+  useEffect(() => {
+    const peerConnection = new PeerConnection(socket);
+
+    socket.on(EVENT.CALL_MADE, peerConnection.onCallMade);
+    socket.on(EVENT.ANSWER_MADE, peerConnection.onAnswerMade);
+    socket.on(EVENT.ICE_CANDIDATE, peerConnection.onIceCandidateReceived);
+    return () => {
+      peerConnection.peerConnection?.close();
+      socket.off(EVENT.CALL_MADE, peerConnection.onCallMade);
+      socket.off(EVENT.ANSWER_MADE, peerConnection.onAnswerMade);
+      socket.off(EVENT.ICE_CANDIDATE, peerConnection.onIceCandidateReceived);
+    };
+  }, []);
 };
 
 export default usePeerConnection;
