dca77896b587f5ee4e701874e4e37889d2026666
김민수 MINSOO KIM <alstn113@gmail.com>
2023-01-04 00:33:40 +0900

    feat: set setVideoTracks

--- packages/client/src/pages/Room/VideoContents.tsx
+++ packages/client/src/pages/Room/VideoContents.tsx
@@ -1,11 +1,49 @@
 import styled from '@emotion/styled';
 import { useEffect, useRef } from 'react';
+import { EVENT } from '~/constants';
+import roomSocket from '~/lib/sockets/roomSocket';
 
 const VideoContents = () => {
-  const localViedoRef = useRef<HTMLVideoElement>(null);
+  const localVideoRef = useRef<HTMLVideoElement>(null);
   const remoteVideoRef = useRef<HTMLVideoElement>(null);
+  const pcRef = useRef<RTCPeerConnection>(null);
+  const socket = roomSocket.socket;
 
   const setVideoTracks = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: {
+          facingMode: 'user',
+        },
+      });
+      if (localVideoRef.current) localVideoRef.current.srcObject = stream;
+      if (!(pcRef.current && socket)) return;
+
+      stream.getTracks().forEach((track) => {
+        if (!pcRef.current) return;
+        pcRef.current.addTrack(track, stream);
+      });
+      pcRef.current.onicecandidate = (e) => {
+        if (e.candidate) {
+          if (!socket) return;
+          console.log('candidate', e.candidate);
+          socket.emit(EVENT.ICE_CANDIDATE, e.candidate);
+        }
+      };
+      pcRef.current.oniceconnectionstatechange = (e) => {
+        console.log('ice connection state change', e);
+      };
+      pcRef.current.ontrack = (e) => {
+        console.log('add track success');
+        if (remoteVideoRef.current) remoteVideoRef.current.srcObject = e.streams[0];
+        socket.emit(EVENT.JOIN_ROOM, {
+          roomId: 'test',
+        });
+      };
+    } catch (e) {
+      console.error(e);
+    }
     return;
   };
 
@@ -23,7 +61,7 @@ const VideoContents = () => {
 
   return (
     <VideoContainer>
-      <VideoScreen autoPlay ref={localViedoRef} />
+      <VideoScreen autoPlay ref={localVideoRef} />
       <VideoScreen autoPlay ref={remoteVideoRef} />
     </VideoContainer>
   );
