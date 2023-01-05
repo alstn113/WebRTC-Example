ca250d5c6238ab23dc8210279d147570c56bea22
김민수 MINSOO KIM <alstn113@gmail.com>
2023-01-05 21:55:04 +0900

    feat: lobby gateway init setting

--- packages/server/src/modules/events/lobby/lobby.gateway.service.ts
+++ packages/server/src/modules/events/lobby/lobby.gateway.service.ts
@@ -1,6 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Server, Socket } from 'socket.io';
+import { AuthService } from '~/modules/auth/auth.service';
+import { UsersRepository } from '~/modules/users/users.repository';
+import { parseCookie } from '~/utils';
 
 @Injectable()
 export class LobbyGatewayService {
-  // constructor() {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userRepository: UsersRepository,
+  ) {}
+
+  private server: Server;
+  private logger = new Logger('LobbyGateway');
+
+  onGatewayInit(server: Server) {
+    this.server = server;
+    this.logger.verbose('Initialized LobbyGateway');
+  }
+
+  async onGatewayConnection(client: Socket) {
+    try {
+      const accessToken = parseCookie(client.handshake.headers.cookie, 'access_token');
+      const accessTokenPayload = await this.authService.verifyToken(accessToken, 'access_token');
+      const user = await this.userRepository.findUserById(accessTokenPayload.userId);
+
+      client.data.user = {
+        id: user.id,
+        email: user.email,
+      };
+    } catch (error) {
+      this.logger.error(error.message);
+      client.disconnect(true);
+    }
+  }
+
+  onGatewayDisconnect(client: Socket) {
+    return;
+  }
 }
--- packages/server/src/modules/events/lobby/lobby.gateway.ts
+++ packages/server/src/modules/events/lobby/lobby.gateway.ts
@@ -1,10 +1,31 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { LobbyGatewayService } from './lobby.gateway.service';
 
 @WebSocketGateway({
   cors: { origin: '*' },
   transports: ['websocket', 'polling'],
   namespace: 'socket/lobby',
 })
-export class LobbyGateway {
-  // constructor() {}
+export class LobbyGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  constructor(private readonly lobbyGatewayService: LobbyGatewayService) {}
+
+  /** Default Setting */
+
+  afterInit(server: Server) {
+    return this.lobbyGatewayService.onGatewayInit(server);
+  }
+
+  async handleConnection(client: Socket) {
+    return await this.lobbyGatewayService.onGatewayConnection(client);
+  }
+
+  handleDisconnect(client: Socket) {
+    return this.lobbyGatewayService.onGatewayDisconnect(client);
+  }
 }
