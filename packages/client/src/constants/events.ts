12dcbdb94a08a583382ab238e1a337a3a0a69bb0
김민수 MINSOO KIM <alstn113@gmail.com>
2023-01-08 01:19:49 +0900

    feat: add events constants

--- packages/client/src/constants/events.ts
+++ packages/client/src/constants/events.ts
@@ -1,11 +1,15 @@
 export const EVENT = {
   /** Socket Room */
   JOIN_ROOM: 'join_room',
+  JOINED_ROOM: 'joined_room',
   LEAVE_ROOM: 'leave_room',
+  LEFT_ROOM: 'left_room',
 
   /** Socket Lobby */
   JOIN_LOBBY: 'join_lobby',
+  JOINED_LOBBY: 'joined_lobby',
   LEAVE_LOBBY: 'leave_lobby',
+  LEFT_LOBBY: 'left_lobby',
 
   /** Socket Chat */
   CHAT_MESSAGE: 'chat_message',
