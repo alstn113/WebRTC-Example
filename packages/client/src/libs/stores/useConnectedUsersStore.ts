b0bf2ff17b872f11b3278c3e18bbf0241856a142
김민수 MINSOO KIM <alstn113@gmail.com>
2023-01-08 03:36:54 +0900

    feat: useConnectedUsersStore

---
+++ packages/client/src/libs/stores/useConnectedUsersStore.ts
@@ -0,0 +1,50 @@
+import produce from 'immer';
+import create from 'zustand';
+
+interface ConnectedUserInfo {
+  sid: string;
+  uid: string;
+}
+
+type States = {
+  userStream: { [key: string]: MediaStream | null };
+  connectedUsers: ConnectedUserInfo[];
+};
+
+type Actions = {
+  setUserStream: ({ sid, stream }: { sid: string; stream: MediaStream }) => void;
+  setConnectedUsers: (connectedUsers: ConnectedUserInfo[]) => void;
+  addConnectedUser: (connectedUser: ConnectedUserInfo) => void;
+  deleteConnectedUser: (sid: string) => void;
+};
+
+const useConnectedUsersStore = create<States & Actions>((set) => ({
+  userStream: {},
+  connectedUsers: [],
+  setUserStream: ({ sid, stream }) =>
+    set(
+      produce((draft: States) => {
+        draft.userStream[sid] = stream;
+      }),
+    ),
+  setConnectedUsers: (connectedUsers) =>
+    set(
+      produce((draft: States) => {
+        draft.connectedUsers = connectedUsers;
+      }),
+    ),
+  addConnectedUser: (connectedUser) =>
+    set(
+      produce((draft: States) => {
+        draft.connectedUsers.push(connectedUser);
+      }),
+    ),
+  deleteConnectedUser: (sid) =>
+    set(
+      produce((draft: States) => {
+        draft.connectedUsers = draft.connectedUsers.filter((user) => user.sid !== sid);
+      }),
+    ),
+}));
+
+export default useConnectedUsersStore;
