c6c1fb545d5201a70ba2dfd83936c364a5e30750
김민수 MINSOO KIM <alstn113@gmail.com>
2023-01-08 05:27:26 +0900

    refactor: usePeerConnection hook

--- packages/client/src/hooks/usePeerConnection.ts
+++ packages/client/src/hooks/usePeerConnection.ts
@@ -1,21 +1,47 @@
 import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { EVENT } from '~/constants';
 import PeerConnection from '~/libs/sockets/PeerConnection';
 import roomSocket from '~/libs/sockets/roomSocket';
 
-const usePeerConnection = () => {
+const usePeerConnection = (roomId: string) => {
+  const socket = roomSocket.socket;
+  const RTCConfig = {
+    iceServers: [
+      {
+        urls: [
+          'stun:stun.l.google.com:19302',
+          'stun:stun1.l.google.com:19302',
+          'stun:stun2.l.google.com:19302',
+          'stun:stun3.l.google.com:19302',
+          'stun:stun4.l.google.com:19302',
+        ],
+      },
+    ],
+  };
   useEffect(() => {
-    const peerConnection = new PeerConnection();
+    const createPeerConnection = (sid: string) => {
+      const peerConnection = new RTCPeerConnection(RTCConfig);
 
-    roomSocket.socket?.on(EVENT.CALL_USER, peerConnection.onCallUser);
-    roomSocket.socket?.on(EVENT.CALL_MADE, peerConnection.onCallMade);
-    roomSocket.socket?.on(EVENT.ANSWER_MADE, peerConnection.onAnswerMade);
-    roomSocket.socket?.on(EVENT.ICE_CANDIDATE, peerConnection.onIceCandidateReceived);
-    return () => {
-      roomSocket.socket?.off(EVENT.CALL_USER, peerConnection.onCallUser);
-      roomSocket.socket?.off(EVENT.CALL_MADE, peerConnection.onCallMade);
-      roomSocket.socket?.off(EVENT.ANSWER_MADE, peerConnection.onAnswerMade);
-      roomSocket.socket?.off(EVENT.ICE_CANDIDATE, peerConnection.onIceCandidateReceived);
+      return;
+    };
+    const createOffer = () => {
+      return;
+    };
+    const createAnswer = () => {
+      return;
+    };
+    const onReceivedOffer = () => {
+      return;
+    };
+    const onReceivedAnswer = () => {
+      return;
+    };
+    const onReceivedIceCandidate = () => {
+      return;
+    };
+    const onReceivedCall = () => {
+      return;
     };
   }, []);
 };
